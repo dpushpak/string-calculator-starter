da06feb81d0879e79b773fcc38f86943fad02b6b
D1_49663_Pushpak <pushpak.dharmadhikari@gmail.com>
2021-10-17 21:55:36 +0530

    for two numbers

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,9 +3,14 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-    	
-    	
-        return 0;
+    	int sum=0;
+    	String[] arr=input.split(",");
+    	if(input.contains(",")) {
+    		for(int i=0; i<arr.length; i++) {
+    			sum=sum+Integer.parseInt(arr[i]);
+    		}
+    	}
+        return sum;
     }
-
+    
 }
